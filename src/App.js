5a554135b2b1a0eb4bef6533119d5048234a9e8d
bonzo <40222661+baramatsubonzo@users.noreply.github.com>
2021-07-17 09:03:40 +0900

    レンダリングの最適化(memo, useCallback)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -11,9 +11,7 @@ export default function App() {
 
   const onClickOpen = () => setOpen(!open);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
 
   return (
     <div className="App">
@@ -21,7 +19,7 @@ export default function App() {
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
